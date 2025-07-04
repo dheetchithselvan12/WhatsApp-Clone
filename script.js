7e2690cee8b34b84bf5f841429cf0242acc7b0ce
dheetchithselvan12 <dheetchithselvan12@gmail.com>
2025-07-04 15:28:29 +0530

    designed: expand menu and contact box

---
+++ script.js
@@ -0,0 +1,7 @@
+let nav = document.getElementById("nav-expand");
+let navbar = document.querySelector(".nav-bar")
+
+nav.addEventListener("click", () => {
+    navbar.classList.toggle("expanded")
+
+})
